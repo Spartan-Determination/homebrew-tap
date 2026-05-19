324fe298f6f0580a987819250a1c4b69fa7cd18b
Spartan-Determination <Spartan-Determination@users.noreply.github.com>
2026-05-19 16:03:25 +0700

    Bump terminalchampion cask SHA256 for v1.0.0-in-place patch

--- Casks/terminalchampion.rb
+++ Casks/terminalchampion.rb
@@ -1,6 +1,6 @@
 cask "terminalchampion" do
   version "1.0.0"
-  sha256 "c2d20bbc9886d5b57e00d4e68ca92d17ba54cc30d946e7e40d898c0e1cc0ff50"
+  sha256 "e9f93346d103af7a9c20cd5bdd41e125ef7e655ea299013ccf62ff8c0ee0a3aa"
 
   url "https://updates.terminalchampion.com/v#{version}/TerminalChampion-#{version}-universal.dmg"
   name "TerminalChampion"
